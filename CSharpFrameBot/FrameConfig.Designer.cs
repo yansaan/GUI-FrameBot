50fab3dccafa0f149a230c1381fe205993e57ac6
Ian Ardiansyah <ian.ian.wold76@gmail.com>
2020-11-07 07:03:56 +0700

    changing layout

--- CSharpFrameBot/FrameConfig.Designer.cs
+++ CSharpFrameBot/FrameConfig.Designer.cs
@@ -29,54 +29,45 @@
     private void InitializeComponent()
     {
       this.listBox1 = new System.Windows.Forms.ListBox();
-      this.label1 = new System.Windows.Forms.Label();
       this.textBox1 = new System.Windows.Forms.TextBox();
       this.label2 = new System.Windows.Forms.Label();
       this.label3 = new System.Windows.Forms.Label();
-      this.label4 = new System.Windows.Forms.Label();
-      this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-      this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
-      this.button1 = new System.Windows.Forms.Button();
-      this.button2 = new System.Windows.Forms.Button();
-      this.label5 = new System.Windows.Forms.Label();
-      this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
-      this.listBox2 = new System.Windows.Forms.ListBox();
-      this.label6 = new System.Windows.Forms.Label();
-      this.label7 = new System.Windows.Forms.Label();
-      this.label8 = new System.Windows.Forms.Label();
-      ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
-      ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
-      ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+      this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+      this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+      this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+      this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+      this.GBpreview = new System.Windows.Forms.GroupBox();
+      this.pictureBox1 = new System.Windows.Forms.PictureBox();
+      ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+      this.splitContainer1.Panel1.SuspendLayout();
+      this.splitContainer1.Panel2.SuspendLayout();
+      this.splitContainer1.SuspendLayout();
+      this.GBpreview.SuspendLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
       this.SuspendLayout();
       // 
       // listBox1
       // 
+      this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
       this.listBox1.FormattingEnabled = true;
-      this.listBox1.Location = new System.Drawing.Point(12, 12);
+      this.listBox1.Location = new System.Drawing.Point(0, 0);
       this.listBox1.Name = "listBox1";
-      this.listBox1.Size = new System.Drawing.Size(288, 407);
+      this.listBox1.Size = new System.Drawing.Size(139, 461);
       this.listBox1.TabIndex = 0;
-      // 
-      // label1
-      // 
-      this.label1.AutoSize = true;
-      this.label1.Location = new System.Drawing.Point(12, 428);
-      this.label1.Name = "label1";
-      this.label1.Size = new System.Drawing.Size(69, 13);
-      this.label1.TabIndex = 1;
-      this.label1.Text = "Total Frame: ";
       // 
       // textBox1
       // 
-      this.textBox1.Location = new System.Drawing.Point(307, 33);
+      this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.textBox1.Location = new System.Drawing.Point(64, 351);
       this.textBox1.Name = "textBox1";
-      this.textBox1.Size = new System.Drawing.Size(481, 20);
+      this.textBox1.Size = new System.Drawing.Size(664, 20);
       this.textBox1.TabIndex = 2;
       // 
       // label2
       // 
       this.label2.AutoSize = true;
-      this.label2.Location = new System.Drawing.Point(304, 12);
+      this.label2.Location = new System.Drawing.Point(10, 354);
       this.label2.Name = "label2";
       this.label2.Size = new System.Drawing.Size(48, 13);
       this.label2.TabIndex = 3;
@@ -85,133 +76,98 @@
       // label3
       // 
       this.label3.AutoSize = true;
-      this.label3.Location = new System.Drawing.Point(306, 63);
+      this.label3.Location = new System.Drawing.Point(10, 393);
       this.label3.Name = "label3";
       this.label3.Size = new System.Drawing.Size(35, 13);
       this.label3.TabIndex = 4;
       this.label3.Text = "label3";
       // 
-      // label4
+      // splitContainer1
       // 
-      this.label4.AutoSize = true;
-      this.label4.Location = new System.Drawing.Point(306, 189);
-      this.label4.Name = "label4";
-      this.label4.Size = new System.Drawing.Size(30, 13);
-      this.label4.TabIndex = 5;
-      this.label4.Text = "Time";
+      this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+      this.splitContainer1.Name = "splitContainer1";
       // 
-      // numericUpDown1
+      // splitContainer1.Panel1
       // 
-      this.numericUpDown1.Location = new System.Drawing.Point(344, 187);
-      this.numericUpDown1.Name = "numericUpDown1";
-      this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
-      this.numericUpDown1.TabIndex = 6;
+      this.splitContainer1.Panel1.Controls.Add(this.listBox1);
       // 
-      // numericUpDown2
+      // splitContainer1.Panel2
       // 
-      this.numericUpDown2.Location = new System.Drawing.Point(554, 187);
-      this.numericUpDown2.Name = "numericUpDown2";
-      this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
-      this.numericUpDown2.TabIndex = 6;
+      this.splitContainer1.Panel2.Controls.Add(this.GBpreview);
+      this.splitContainer1.Panel2.Controls.Add(this.toolStrip1);
+      this.splitContainer1.Panel2.Controls.Add(this.textBox1);
+      this.splitContainer1.Panel2.Controls.Add(this.label2);
+      this.splitContainer1.Panel2.Controls.Add(this.label3);
+      this.splitContainer1.Size = new System.Drawing.Size(895, 461);
+      this.splitContainer1.SplitterDistance = 139;
+      this.splitContainer1.TabIndex = 10;
       // 
-      // button1
+      // menuStrip1
       // 
-      this.button1.Location = new System.Drawing.Point(313, 395);
-      this.button1.Name = "button1";
-      this.button1.Size = new System.Drawing.Size(75, 23);
-      this.button1.TabIndex = 7;
-      this.button1.Text = "Start";
-      this.button1.UseVisualStyleBackColor = true;
+      this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+      this.menuStrip1.Name = "menuStrip1";
+      this.menuStrip1.Size = new System.Drawing.Size(895, 24);
+      this.menuStrip1.TabIndex = 11;
+      this.menuStrip1.Text = "menuStrip1";
       // 
-      // button2
+      // toolStrip1
       // 
-      this.button2.Location = new System.Drawing.Point(395, 395);
-      this.button2.Name = "button2";
-      this.button2.Size = new System.Drawing.Size(75, 23);
-      this.button2.TabIndex = 8;
-      this.button2.Text = "Stop";
-      this.button2.UseVisualStyleBackColor = true;
+      this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+      this.toolStrip1.Name = "toolStrip1";
+      this.toolStrip1.Size = new System.Drawing.Size(752, 25);
+      this.toolStrip1.TabIndex = 5;
+      this.toolStrip1.Text = "toolStrip1";
       // 
-      // label5
+      // statusStrip1
       // 
-      this.label5.AutoSize = true;
-      this.label5.Location = new System.Drawing.Point(304, 123);
-      this.label5.Name = "label5";
-      this.label5.Size = new System.Drawing.Size(73, 13);
-      this.label5.TabIndex = 5;
-      this.label5.Text = "Current Frame";
+      this.statusStrip1.Location = new System.Drawing.Point(0, 485);
+      this.statusStrip1.Name = "statusStrip1";
+      this.statusStrip1.Size = new System.Drawing.Size(895, 22);
+      this.statusStrip1.TabIndex = 12;
+      this.statusStrip1.Text = "statusStrip1";
       // 
-      // numericUpDown3
+      // GBpreview
       // 
-      this.numericUpDown3.Location = new System.Drawing.Point(307, 139);
-      this.numericUpDown3.Name = "numericUpDown3";
-      this.numericUpDown3.Size = new System.Drawing.Size(120, 20);
-      this.numericUpDown3.TabIndex = 6;
+      this.GBpreview.Controls.Add(this.pictureBox1);
+      this.GBpreview.Dock = System.Windows.Forms.DockStyle.Top;
+      this.GBpreview.Location = new System.Drawing.Point(0, 25);
+      this.GBpreview.Name = "GBpreview";
+      this.GBpreview.Size = new System.Drawing.Size(752, 308);
+      this.GBpreview.TabIndex = 6;
+      this.GBpreview.TabStop = false;
+      this.GBpreview.Text = "Preview";
       // 
-      // listBox2
+      // pictureBox1
       // 
-      this.listBox2.FormattingEnabled = true;
-      this.listBox2.Location = new System.Drawing.Point(307, 243);
-      this.listBox2.Name = "listBox2";
-      this.listBox2.Size = new System.Drawing.Size(481, 147);
-      this.listBox2.TabIndex = 9;
-      // 
-      // label6
-      // 
-      this.label6.AutoSize = true;
-      this.label6.Location = new System.Drawing.Point(470, 189);
-      this.label6.Name = "label6";
-      this.label6.Size = new System.Drawing.Size(44, 13);
-      this.label6.TabIndex = 5;
-      this.label6.Text = "Minutes";
-      // 
-      // label7
-      // 
-      this.label7.AutoSize = true;
-      this.label7.Location = new System.Drawing.Point(680, 189);
-      this.label7.Name = "label7";
-      this.label7.Size = new System.Drawing.Size(49, 13);
-      this.label7.TabIndex = 5;
-      this.label7.Text = "Seconds";
-      this.label7.Click += new System.EventHandler(this.label7_Click);
-      // 
-      // label8
-      // 
-      this.label8.AutoSize = true;
-      this.label8.Location = new System.Drawing.Point(306, 227);
-      this.label8.Name = "label8";
-      this.label8.Size = new System.Drawing.Size(25, 13);
-      this.label8.TabIndex = 5;
-      this.label8.Text = "Log";
-      this.label8.Click += new System.EventHandler(this.label7_Click);
+      this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.pictureBox1.Location = new System.Drawing.Point(51, 33);
+      this.pictureBox1.Name = "pictureBox1";
+      this.pictureBox1.Size = new System.Drawing.Size(644, 247);
+      this.pictureBox1.TabIndex = 0;
+      this.pictureBox1.TabStop = false;
       // 
       // FrameConfig
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(800, 450);
-      this.Controls.Add(this.listBox2);
-      this.Controls.Add(this.button2);
-      this.Controls.Add(this.button1);
-      this.Controls.Add(this.numericUpDown2);
-      this.Controls.Add(this.numericUpDown3);
-      this.Controls.Add(this.label5);
-      this.Controls.Add(this.numericUpDown1);
-      this.Controls.Add(this.label8);
-      this.Controls.Add(this.label7);
-      this.Controls.Add(this.label6);
-      this.Controls.Add(this.label4);
-      this.Controls.Add(this.label3);
-      this.Controls.Add(this.label2);
-      this.Controls.Add(this.textBox1);
-      this.Controls.Add(this.label1);
-      this.Controls.Add(this.listBox1);
+      this.ClientSize = new System.Drawing.Size(895, 507);
+      this.Controls.Add(this.splitContainer1);
+      this.Controls.Add(this.statusStrip1);
+      this.Controls.Add(this.menuStrip1);
+      this.MainMenuStrip = this.menuStrip1;
       this.Name = "FrameConfig";
       this.Text = "FrameConfig";
       this.Load += new System.EventHandler(this.FrameConfig_Load);
-      ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
-      ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
-      ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+      this.splitContainer1.Panel1.ResumeLayout(false);
+      this.splitContainer1.Panel2.ResumeLayout(false);
+      this.splitContainer1.Panel2.PerformLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+      this.splitContainer1.ResumeLayout(false);
+      this.GBpreview.ResumeLayout(false);
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
       this.ResumeLayout(false);
       this.PerformLayout();
 
@@ -220,20 +176,14 @@
     #endregion
 
     private System.Windows.Forms.ListBox listBox1;
-    private System.Windows.Forms.Label label1;
     private System.Windows.Forms.TextBox textBox1;
     private System.Windows.Forms.Label label2;
     private System.Windows.Forms.Label label3;
-    private System.Windows.Forms.Label label4;
-    private System.Windows.Forms.NumericUpDown numericUpDown1;
-    private System.Windows.Forms.NumericUpDown numericUpDown2;
-    private System.Windows.Forms.Button button1;
-    private System.Windows.Forms.Button button2;
-    private System.Windows.Forms.Label label5;
-    private System.Windows.Forms.NumericUpDown numericUpDown3;
-    private System.Windows.Forms.ListBox listBox2;
-    private System.Windows.Forms.Label label6;
-    private System.Windows.Forms.Label label7;
-    private System.Windows.Forms.Label label8;
+    private System.Windows.Forms.SplitContainer splitContainer1;
+    private System.Windows.Forms.GroupBox GBpreview;
+    private System.Windows.Forms.PictureBox pictureBox1;
+    private System.Windows.Forms.ToolStrip toolStrip1;
+    private System.Windows.Forms.MenuStrip menuStrip1;
+    private System.Windows.Forms.StatusStrip statusStrip1;
   }
 }
